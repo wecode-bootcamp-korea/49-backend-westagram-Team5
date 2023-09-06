248cc554d5b667a4bf9d064eedc64c4b132c5c22
Seungwon Kim <kim.sw94@gmail.com>
2023-09-06 21:09:45 +0900

    yaho

--- seungwon/app.js
+++ seungwon/app.js
@@ -3,8 +3,6 @@ const express = require('express')
 const { DataSource } = require('typeorm');
 const app = express();
 
-const server = http.createServer(app)
-
 app.use(express.json()) // for parsing application/json
 
 const server = http.createServer(app) // express app 으로 서버를 만듭니다.
@@ -17,7 +15,7 @@ const start = async () => { // 서버를 시작하는 함수입니다.
   }
 }
 
-start ();
+start();
 
 app.get("users", async(req, res) => {
   try {
